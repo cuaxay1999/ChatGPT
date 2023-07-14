dde6c9478f604077ede72812ed2599f74e20ac6a
Quang Anh Đặng <cuaxay1999@gmail.com>
2023-07-14 11:50:58 +0700

    final

---
+++ utils/firebaseHelper.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+export const getFirebaseApp = () => {
+    const firebaseConfig = {
+       // your config goes here
+    }
+
+    // Initialize Firebase
+    return initializeApp(firebaseConfig)
+}
